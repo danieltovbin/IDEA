ff0f6fc389779fc7ea53d26003afc4b98b35657c
danieltovbin <tovbindaniel@gmail.com>
2024-03-13 12:00:31 +0200

    moment library

--- client/src/components/projectPage/TabPanels/Comments.tsx
+++ client/src/components/projectPage/TabPanels/Comments.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { getDonationsByProject } from '../../../API/Donation/donationClientCtl'
 import { Donation } from '../../../vite-env'
-import './scss/comments.scss'
+import './scss/comments.scss';
+import moment from 'moment';
+import 'moment-timezone';
 
 const Comments = () => {
     const [donations, setdonations] = useState<Donation[]>([])
@@ -26,6 +28,7 @@ const Comments = () => {
         allDonationsByProject();
     },[])
 
+  
   return (
     <div>
         <div style={{paddingBottom: '8px'}}>
@@ -43,7 +46,8 @@ const Comments = () => {
                 </div>
                 <div style={{ padding: '0px 14px'}}>
                     <h2 style={{fontSize: '13px', padding:0}}>{donate.name}</h2>
-                    <p style={{fontSize: '11px', color: 'rgb(125, 125, 125)'}}>הגיב לפני שעות</p>
+                    <p style={{fontSize: '11px', color: 'rgb(125, 125, 125)'}}>   הגיב לפני {formatTimeDifference(donate.date)}
+                    </p>
                 </div>
             </div>
             <div className='comment' style={{backgroundColor: 'rgba(214, 214, 214, 0.5)'}}><span style={{fontSize: '13px',marginRight: '6px',overflow:'hidden'}}>{donate.comment}</span></div>
@@ -57,3 +61,28 @@ const Comments = () => {
 
 export default Comments
 
+
+
+ 
+const formatTimeDifference = (date: string) => {
+    const now = moment();
+    const commentDate = moment(date, moment.ISO_8601).tz(moment.tz.guess());
+    console.log('now:', now);
+  console.log('commentDate:', commentDate);
+    const diffInSeconds = now.diff(commentDate, 'seconds');
+    console.log('diffInSeconds:', diffInSeconds);
+  
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds} שניות`;
+    } else if (diffInSeconds < 3600) {
+      return `${Math.floor(diffInSeconds / 60)} דקות`;
+    } else if (diffInSeconds < 86400) {
+      return `${Math.floor(diffInSeconds / 3600)} שעות`;
+    } else if (diffInSeconds < 2592000) {
+      return `${Math.floor(diffInSeconds / 86400)} ימים`;
+    } else if (diffInSeconds < 31536000) {
+      return `${Math.floor(diffInSeconds / 2592000)} חודשים`;
+    } else {
+      return `${Math.floor(diffInSeconds / 31536000)} שנים`;
+    }
+  };
