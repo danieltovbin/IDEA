5e4f9ea48f3eb7cbb17d4e46aae7ef5326f2ac07
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2024-03-21 19:22:31 +0200

    .

--- client/src/API/Projects/projectClientCtrl.ts
+++ client/src/API/Projects/projectClientCtrl.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export let environment = "DEV";
 const CLIENT_DEV_URL = "http://localhost:3000";
 const CLIENT_PROD_URL = "https://idea-getappyourstartup-server.onrender.com";
+// const CLIENT_PROD_URL = "https://idea-getappyourstartup-server.onrender.com";
 
 environment = environment === "DEV" ? CLIENT_DEV_URL : CLIENT_PROD_URL;
 
